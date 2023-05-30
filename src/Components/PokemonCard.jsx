5bfa1dd9dfdaf4f31068314839a7b00b095c8ee0
Denis CALAMAND <calamanddenis@gmail.com>
2023-05-30 18:04:46 +0200

    creation composant 'PokemonCard' dans la branche components.1

---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+function PokemonImag () {
+  return (
+    // <div>
+      <figure>
+        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"></img>
+        <figcaption>bulbasaur</figcaption>
+      </figure>
+    // </div>
+    );
+}
+export default PokemonImag;
