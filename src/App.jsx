10a627f70f7f666bd6fb4e5536bd4ca14c743b0b
Denis CALAMAND <calamanddenis@gmail.com>
2023-06-01 16:49:51 +0200

    React Basics 06 - Pokédex - Utiliser un state

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import './App.css';
 import PokemonCard from "./Components/PokemonCard";
+import { useState } from "react";
 
 function App() {
   const pokemonList = [
     {
-      name:  "bulbasaur", // "bulbasaur"
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
   ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickS = () => (pokemonIndex < pokemonList.length - 1) ? setPokemonIndex(pokemonIndex + 1) : null
+  const handleClickP = () => (pokemonIndex > 0) ? setPokemonIndex(pokemonIndex - 1) : null
 
   return (
     <>
       <div>
-        { <PokemonCard pokemon={pokemonList[0]}/> }
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+        <button onClick={handleClickP}>Précedent</button>
+        <button onClick={handleClickS}>Suivant</button>
       </div>
     </>
   )
 }
-
 export default App
